2a1521544acee26c5faa185e72a8d72a59629599
Michelle Santiago <michellesantiagobeadle@hotmail.com>
2018-12-15 03:40:51 -0200

    Movimentação certinha

--- New Unity Project/Assets/Scripts/Player/PlayerMovement.cs
+++ New Unity Project/Assets/Scripts/Player/PlayerMovement.cs
@@ -7,11 +7,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
-        var z = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+        var z = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
 
-        transform.Rotate(0, x, 0);
-        transform.Translate(0, 0, z);
+        transform.Rotate(0, 0, z);
+        transform.Translate(0, -y, 0);
 
     }
 }
