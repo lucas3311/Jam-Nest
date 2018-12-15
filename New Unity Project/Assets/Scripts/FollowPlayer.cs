3f14676c783af34ba08a5ddb2b4179d861f0734f
Michelle Santiago <michellesantiagobeadle@hotmail.com>
2018-12-15 01:38:41 -0200

    Cinemachine

---
+++ New Unity Project/Assets/Scripts/FollowPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    private GameObject target;
+    Vector3 offset;
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+
+        transform.LookAt(target.transform);
+        offset = transform.position - target.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        Vector3 desiredPosition = target.transform.position + offset;
+        transform.position = desiredPosition;
+    }
+}
