9edb13195860ffd066d0ddc172f41d0b256b65b0
lucas3311 <lucaslimareis3311@gmail.com>
2018-12-15 04:40:31 -0200

    asdad

---
+++ New Unity Project/Assets/Scripts/Controls.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controls : MonoBehaviour
+{
+    public GameObject body;
+    public GameObject spirit;
+
+    private GameObject currentState;
+
+    private OutOfBodyExperience changeStateScript;
+
+
+    void Start()
+    {
+        if (body == null)
+        {
+            body = GameObject.FindGameObjectWithTag("Player");
+        } else if (spirit == null)
+        {
+            spirit = GameObject.FindGameObjectWithTag("Spirit");
+        }
+
+        currentState = body;
+
+        changeStateScript = this.GetComponent<OutOfBodyExperience>();
+    }
+
+    void LateUpdate()
+    {
+        // Movimentação
+        var z = Input.GetAxis("Horizontal") * Time.deltaTime * 150.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        currentState.transform.Rotate(0, 0, z);
+        currentState.transform.Translate(0, -y, 0);
+
+        // Mecânica do espírito
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            if(currentState == body)
+            {
+                changeStateScript.ReleaseSpirit(spirit, body);
+                currentState = spirit;
+
+            } else if (currentState == spirit)
+            {
+                changeStateScript.RetrieveSpirit(body, spirit);
+                currentState = body;
+            }
+        }
+    }
+}
---
+++ New Unity Project/Assets/Scripts/Player/SpiritProperties.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpiritProperties : MonoBehaviour
+{
+    public GameObject camera;
+}
